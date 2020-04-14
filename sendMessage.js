fda830e5d00fa1e2ca558b8d7a492676d6858f56
Dani Latorre <dani@danilat.com>
2020-04-14 21:07:53 +0200

    Improving tests and now we are managing exceptions like network errors getting the notification content

--- sendMessage.js
+++ sendMessage.js
@@ -7,20 +7,26 @@ async function sendMessage(){
   const token = core.getInput("token");
   const to = core.getInput("to");
   const parse_mode = core.getInput("parse-mode");
-  let response = await httpClient.get(contentUrl)
-  let message = encodeURI(await response.readBody())
-  console.log("Message to send to telegram:", message)
-  console.log("Sending message to send:", to)
-  const telegramEndopoint = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${to}&text=${message}&parse_mode=${parse_mode}`
-  const telegramResponse = await httpClient.post(telegramEndopoint)
-  const telegramMessage = await telegramResponse.readBody()
-  console.log("Telegrams response:", telegramMessage)
-  if (telegramResponse.message.statusCode != 200) {
-    core.setFailed("Telegram FAILED", telegramMessage);
-  } else {
-    core.setOutput("Telegrams SUCCESS", telegramMessage);
+
+  try {
+    let response = await httpClient.get(contentUrl)
+  
+    let message = encodeURI(await response.readBody())
+    console.log("Message to send to telegram:", message)
+    console.log("Sending message to send:", to)
+    const telegramEndopoint = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${to}&text=${message}&parse_mode=${parse_mode}`
+    const telegramResponse = await httpClient.post(telegramEndopoint)
+    const telegramMessage = await telegramResponse.readBody()
+    console.log("Telegrams response:", telegramMessage)
+    if (telegramResponse.message.statusCode != 200) {
+      core.setFailed("Telegram FAILED", telegramMessage);
+    } else {
+      core.setOutput("Telegrams SUCCESS", telegramMessage);
+    }
+    return telegramResponse;
+  } catch(error){
+    core.setFailed("Some error happened", error);
   }
-  return telegramResponse;
 
 }
 
--- tests/actions.test.js
+++ tests/actions.test.js
@@ -3,20 +3,65 @@ const core = require("@actions/core");
 const sendMessage = require("../sendMessage");
 require("dotenv").config()
 
+describe("run the action", () =>{
+  let failedSpy;
+  beforeEach(() => {
+    core.getInput = (argName) => {
+      switch (argName) {
+        case "to":
+          return process.env.TO;
+        case "content-url":
+          return process.env.URL;
+        case "token":
+          return process.env.TOKEN;
+          case "parse-mode":
+            return "html";
+      }
+    };
+    failedSpy = jest.spyOn(core, "setFailed");
+  });
 
-test("run the action", async () => {
-  core.getInput = (argName) => {
-    switch (argName) {
-      case "to":
-        return process.env.TO;
-      case "content-url":
-        return process.env.URL;
-      case "token":
-        return process.env.TOKEN;
-        case "parse-mode":
-          return "html";
-    }
-  }; 
-  const reponse = await sendMessage()
-  expect(reponse.message.statusCode).toBe(200);
-});
+  afterEach(() => {    
+    jest.clearAllMocks();
+  });
+
+  describe("when the configuration is correct", () =>{
+    test("the notification is sent", async () => {
+      const reponse = await sendMessage()
+
+      expect(reponse.message.statusCode).toBe(200);
+    });
+  });
+
+  describe("when the TOKEN configuration is incorrect", () =>{
+    test("the notification is not sent", async () => {
+      process.env.TOKEN = "foo"
+
+      const reponse = await sendMessage()
+
+      expect(reponse.message.statusCode).toBe(404);
+      expect(failedSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe("when the TO configuration is incorrect", () =>{
+    test("the notification is not sent", async () => {
+      process.env.TO = "foo"
+
+      const reponse = await sendMessage()
+
+      expect(reponse.message.statusCode).toBe(404);
+      expect(failedSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe("when the URL configuration is incorrect", () =>{
+    test("the notification is not sent", async () => {
+      process.env.URL = "blabla"
+
+      const reponse = await sendMessage()
+
+      expect(failedSpy).toHaveBeenCalled();
+    });
+  });
+})
